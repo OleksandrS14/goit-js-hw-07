9a9c723fcf70b363ada0dcdb1d00dbfc65a063d6
Oleksandr1451 <alekszzz154@gmail.com>
2024-09-08 17:11:44 +0300

    update index.html

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('#controls input[type="number"]');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+function createBoxes(amount) {
+  const boxes = []; 
+
+  for (let i = 0; i < amount; i++) {
+      const size = 30 + i * 10; 
+      const box = document.createElement('div');
+      box.style.width = `${size}px`; 
+      box.style.height = `${size}px`; 
+      box.style.backgroundColor = getRandomHexColor(); 
+      boxes.push(box); 
+  }
+
+  boxesContainer.append(...boxes); 
+}
+
+createButton.addEventListener('click', () => {
+  const amount = Number(input.value); 
+  
+  if (amount < 1 || amount > 100) {
+      return; 
+  }
+
+  boxesContainer.innerHTML = ''; 
+  createBoxes(amount); 
+  input.value = ''; 
+});
+
+destroyButton.addEventListener('click', () => {
+  boxesContainer.innerHTML = ''; 
+});
